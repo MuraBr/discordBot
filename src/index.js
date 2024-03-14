deda4f854ba60e7b17ca5b6744b090b368e3daed
MuraBr <88625381+MuraBr@users.noreply.github.com>
2024-03-14 12:43:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config(); //initializes dotenv
-const axios = require('axios'); //add this line at the top
-const { Client, GatewayIntentBits } = require('discord.js'); //imports discord.js
+require('dotenv').config();
+const axios = require('axios');
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -13,25 +13,6 @@ let server = client.guilds.cache.get('<guild id>');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-});
-
-async function getMeme() {
-    const res = await axios.get('https://memeapi.pythonanywhere.com/');
-    return res.data.memes[0].url;
-}
-
-client.on('message', async msg => {
-    switch (msg.content) {
-        case "ping":
-            msg.reply("Pong!");
-            break;
-        //starting meme command
-        case "!meme":
-            msg.channel.send("Here's your meme!"); //the reply to the user command
-            const img = await getMeme(); //fetches the URL from the API
-            msg.channel.send(img); //sends the image URL to Discord
-            break;
-    }
 });
 
 client.on('interactionCreate', (interaction) => {
@@ -53,7 +34,7 @@ client.on('interactionCreate', (interaction) => {
         }
     }
     if(interaction.commandName === 'ms') {
-        interaction.reply(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        interaction.reply(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 });
 
