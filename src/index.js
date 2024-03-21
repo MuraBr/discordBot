04aa67b69bf253838dded19d557864588d31fa70
MuraBr <kenjimurakami449@gmail.com>
2024-03-20 22:52:35 -0400

    oi mate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
 
 const client = new Client({
@@ -10,24 +10,7 @@ const client = new Client({
         GatewayIntentBits.GuildMessages
     ],
 });
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
-
+ 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
